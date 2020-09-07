b46086adf4b7e868c60b3dd0d08270b28fa176ef
codypronnier76130 <codypronnier@gmail.com>
2020-09-07 16:54:45 +0200

    //Dimension svg

--- src/Header.js
+++ src/Header.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { ReactComponent as Logo } from './img/logo_3.svg';
 
+
+function Header() {
+
+ return(
+          <div>
+            {/* Logo is an actual React component */}
+            <Logo />
+          </div>
+        );
+      }
+
+
+export default Header

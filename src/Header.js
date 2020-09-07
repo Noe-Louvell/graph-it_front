af2fbf14a9e056bb8c0e36f0fb19823923a9e0e6
codypronnier76130 <codypronnier@gmail.com>
2020-09-07 17:20:41 +0200

    ajout menu 1.0

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { ReactComponent as Logo } from './assets/img/logo_3.svg';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 
 function Header() {
 
  return(
           <div>
-          <h3>Equipe</h3>
-            {/* Logo is an actual React component */}
-            <Logo />
+          <Nav defaultActiveKey="/home" as="ul">
+              <Nav.Item as="li">
+              <Nav.Link href="/home">Equipe</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+              <Nav.Link eventKey="link-1">Article</Nav.Link>
+              </Nav.Item>
+          <Nav.Item as="li">
+              <Nav.Link eventKey="link-2"><Logo/></Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+              <Nav.Link eventKey="link-1">Réalisations</Nav.Link>
+              </Nav.Item>
+              <Nav.Item as="li">
+                  <Nav.Link eventKey="link-1">Nous contacter</Nav.Link>
+                  </Nav.Item>
+          </Nav>
           </div>
         );
       }

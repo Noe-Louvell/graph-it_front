4472e03d8f052ee19409f03fee8846569f86c325
Noe-Louvell <noe.louvel@viacesi.fr>
2020-09-09 10:06:42 +0200

    Header page équipe

--- src/pages/Equipe/Equipe.js
+++ src/pages/Equipe/Equipe.js
@@ -4,25 +4,84 @@ import {ReactComponent as Footer} from '../../assets/img/fond_footer-couleur.svg
 
 function Equipe() {
   return (
-    <div>
 
 
+<main>
 
-
-
-
-
-
+  <section className="Background HeaderPage">
+    <div className="contenair">
       <div className="row">
-        <div className="boxEquipe">
-          <div className="personne">
-            <h2>Test</h2>
-          </div>
-
-        </div >
-
+        <div className="zoneText">
+          <h1> Notre équipe </h1>
+          <h2> Etuidant du cesi </h2>
+        </div>
       </div>
     </div>
+  </section>
+
+  <div className="contenair">
+    <div className="row description">
+      <div className="descriptionText">
+        <h1>Le cesi</h1>
+        <p>statut étudiant à cesi</p>
+
+      </div>
+      <div className="descriptionImg">
+        <h1>logo cesi.png</h1>
+      </div>
+    </div>
+  </div>
+  <div className="contenair">
+    <div className="row description">
+      <div className="descriptionText">
+        <h1>Le cesi</h1>
+        <p>statut étudiant à cesi</p>
+
+      </div>
+      <div className="descriptionImg">
+        <h1>logo cesi.png</h1>
+      </div>
+    </div>
+  </div>
+  <div className="contenair">
+    <div className="row description">
+      <div className="descriptionText">
+        <h1>Le cesi</h1>
+        <p>statut étudiant à cesi</p>
+
+      </div>
+      <div className="descriptionImg">
+        <h1>logo cesi.png</h1>
+      </div>
+    </div>
+  </div>
+  <div className="contenair">
+    <div className="row description">
+      <div className="descriptionText">
+        <h1>Le cesi</h1>
+        <p>statut étudiant à cesi</p>
+
+      </div>
+      <div className="descriptionImg">
+        <h1>logo cesi.png</h1>
+      </div>
+    </div>
+  </div>
+  <div className="contenair">
+    <div className="row description">
+      <div className="descriptionText">
+        <h1>Le cesi</h1>
+        <p>statut étudiant à cesi</p>
+
+      </div>
+      <div className="descriptionImg">
+        <h1>logo cesi.png</h1>
+      </div>
+    </div>
+  </div>
+  
+
+</main>
 
 
 

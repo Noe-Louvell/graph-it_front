4f2430f4b2876a2fc2dd23e7e25c07a5a5dc0700
Noe-Louvell <noe.louvel@viacesi.fr>
2020-09-10 10:15:08 +0200

    Ajout de GetArticle

---
+++ src/components/GetArticle/getArticle.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './getArticle.css';
+
+
+
+function GetArticle() {
+
+
+
+  const [data, setData] = useState(null);
+ function getArticleFunction() {
+
+
+         fetch('http://graph-it.cesi.group/Articles', {
+             method: 'GET',
+             dataType: 'json',
+             headers: {
+                 Accept: 'application/json',
+                 'Content-Type': 'application/json',
+             },
+         })
+             .then((response) => {
+                 response.json()
+                     .then((responseData) => {
+                       //console.log(responseData)
+                         setData(responseData)
+                     });
+             })
+             .catch(function (err) {
+                 //console.log(err)
+             })
+     }
+     var ViewContent = [];
+     console.log(data);
+
+
+   if (data != null) {
+       for (var i = 0; i < data.length; i++) {
+           //console.log(data[i].Photo.url)
+           ViewContent.push(
+
+
+
+                    </div>
+
+                      <div className="CardText">
+                        <p className="Nom"> {data[i].TitrA}</p>
+
+
+                      </div>
+
+
+                   </div>
+
+           )
+       }
+   }
+   else {
+       getArticleFunction();}
+
+
+  return (
+
+
+
+            <div className="boxPersonne">
+
+            {ViewContent}
+
+            </div>
+
+  );
+}
+
+
+export default GetArticle

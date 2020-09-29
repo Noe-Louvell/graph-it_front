b92072acb8ad049086c93adc372c0def128d55ad
codypronnier76130 <codypronnier@gmail.com>
2020-09-29 23:43:35 +0200

    fixed Barre

--- src/pages/Clients/Clients.js
+++ src/pages/Clients/Clients.js
@@ -130,7 +130,7 @@ export default function Dashboard() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+      <AppBar position="fixed" className={clsx(classes.appBar, open && classes.appBarShift)}>
         <Toolbar className={classes.toolbar}>
           <IconButton
             edge="start"
